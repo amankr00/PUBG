b99cfd4dddb8518cd66c233a311d43a8b1adaa10
Aman Kumar <user2amankr@gmail.com>
2024-06-12 15:30:31 +0530

    Thi Commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         
       initialRoute: '/',
       routes: {
-        '/': (context) => VideoPlayerPage(),
+        '/': (context) => HomeMain(),
       
       },
     );
